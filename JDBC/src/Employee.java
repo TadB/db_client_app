e5d021e386029008087f4a014cae3fa86118ca88
TadB <tadeusz.brzytwa@gmail.com>
2018-04-27 10:59:59 +0200

    wstepna implementacja metody getEmployee klasy EmployeesDAL

---
+++ JDBC/src/Employee.java
@@ -0,0 +1,81 @@
+import java.time.LocalDate;
+
+public class Employee {
+	int employeeId;
+    String firstName, lastName, email, phone, jobId;
+	LocalDate hireDate;
+	int managerId, departmentId;
+    int salary;
+
+
+
+	public Employee(){}
+    public Employee(int employeeId){
+        this.employeeId = employeeId;
+    }
+
+    public void add(Employee employee) {
+
+    }
+    public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public int getManagerId() {
+		return managerId;
+	}
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+	public int getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+	
+    public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public LocalDate getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(LocalDate localDate) {
+		this.hireDate = localDate;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+}
--- JDBC/src/EmployeesDAL.java
+++ JDBC/src/EmployeesDAL.java
@@ -1,4 +1,55 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class EmployeesDAL {
-
+	private SQLException ex;
+    Connection connect;
+    public SQLException getSQLException() {
+        return ex;
+    }
+    public EmployeesDAL(Connection connectionInfo){
+        connect= connectionInfo;
+    }
+    public Employee getEmployees(){
+        Employee employees = new Employee();
+        try(Statement statement=connect.createStatement();){
+            String query = "SELECT * FROM EMPLOYEES";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                employees.add(rs2Employee(resultSet));
+            }
+        }
+        catch(SQLException ex){System.out.println(ex);}
+        return employees;
+    }
+    public int udpateEmployee(Employee emp){
+        try (Statement statement =
+            connect.createStatement();){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPatter("yyyyMMdd");    
+        }
+    }
+    private Employee rs2Employee(ResultSet resultSet){
+        Employee emp=null;
+        try{
+            int col=1;
+            emp = new Employee(resultSet.getInt(col++));
+            emp.setEmployeeId(resultSet.getInt(col++));
+            emp.setFirstName(resultSet.getNString(col++));
+            emp.setLastName(resultSet.getNString(col++));
+            emp.setEmail(resultSet.getNString(col++));
+            emp.setPhone(resultSet.getNString(col++));
+            emp.setHireDate(resultSet.getDate(col++).toLocalDate());
+            emp.setJobId(resultSet.getNString(col++));
+            emp.setSalary(resultSet.getInt(col++));
+            col++;
+            emp.setManagerId(resultSet.getInt(col++));
+            emp.setDepartmentId(resultSet.getInt(col++));
+        }
+        catch(SQLException ex){
+            this.ex=ex;
+        }
+        return emp;
+    }
 }
